f4040aecf6d643242679e0eb26f9c66947d9e398
Warayut Taekrathok <warayut2555@gmail.com>
2019-03-20 00:21:10 +0700

    Update update.php

--- page/accout/update.php
+++ page/accout/update.php
@@ -1,141 +1,25 @@
-<div class="card">
-  <div class="card-header">
+<div class="card"><div class="card-header"><h1>เเก้ไขข้อมูลข้อมูลลูกค้า <h1></div><div class="card-body"><form role="form"method="post"action="index.php?content=accout&action=edit"enctype="multipart/form-data"><div class="row"><div class="col-lg-9"><div class="row"><div class="col-lg-3"><div class="form-group"><label>username <font color="#F00"><b>*</b></font></label><input id="member_username"name="member_username"class="form-control"
+value="<?php echo $member['member_username']?>"autocomplete="off"></div></div><div class="col-lg-3"><div class="form-group"><label>password <font color="#F00"><b>*</b></font></label><input id="member_password"name="member_password"class="form-control"
+value="<?php echo $member['member_password']?>"autocomplete="off"></div></div><div class="col-lg-3"><div class="form-group"><label>ประเภท <font color="#F00"><b>*</b></font></label><input id="member_typemember"name="member_typemember"class="form-control"
+value="<?php echo $member['member_typemember']?>"autocomplete="off"></div></div><div class="col-lg-3"><div class="form-group"><label>เพศ <font color="#F00"><b>*</b></font></label><input id="member_sex"name="member_sex"class="form-control"
+value="<?php echo $member['member_sex']?>"autocomplete="off"></div></div></div><div class="row"><div class="col-lg-4"><div class="form-group"><label>ชื่อ <font color="#F00"><b>*</b></font></label><input id="member_firstname"name="member_firstname"class="form-control"
+value="<?php echo $member['member_firstname']?>"autocomplete="off"></div></div><div class="col-lg-4"><div class="form-group"><label>นามสกุล <font color="#F00"><b>*</b></font></label><input id="member_lastname"name="member_lastname"class="form-control"
+value="<?php echo $member['member_lastname']?>"autocomplete="off"></div></div><div class="col-lg-4"><div class="form-group"><label>เบอร์โทรศัพท์ </label><input id="member_tel"name="member_tel"class="form-control"
+value="<?php echo $member['member_tel']?>"autocomplete="off"></div></div></div><div class="row"><div class="col-lg-12"><div class="form-group"><label>วันเกิด </label><input id="member_birthday"name="member_birthday"class="form-control"
+value="<?php echo $member['member_birthday']?>"autocomplete="off"></div></div></div><div class="row"><div class="col-lg-6"><div class="form-group"><label>วันที่เริ่ม </label><input id="member_start"name="member_start"class="form-control"
+value="<?php echo $member['member_start']?>"autocomplete="off"></div></div><div class="col-lg-6"><div class="form-group"><label>วันที่หมดอายุ </label><input id="member_expiry"name="member_expiry"class="form-control"
+value="<?php echo $member['member_expiry']?>"autocomplete="off"></div></div></div></div></div><div class="row"><div class="col-lg-12"><div class="form-group"><label>ที่อยู่ <font color="#F00"><b>*</b></font></label><textarea type="text"id="member_address"name="member_address"
+class="form-control"><?php echo $member['member_address']?></textarea><p class="help-block">Example : 271/55.</p></div></div></div>< !-- /.row (nested) --><div class="row"><div class="col-lg-3"><div class="form-group"><?PHP $trainer=$member_model->getUserByTrainer();
+?><select class="form-control  custom-select"id="member_keeper"name="member_keeper"><?PHP for ($i=0;
+$i < count($trainer);
 
-  <h1>
-  เเก้ไขข้อมูลข้อมูลลูกค้า
-  <h1>
-  </div>
-  <div class="card-body">
+$i++) {
+    ?><option value="<?PHP echo $trainer[$i]['member_id'];?>"<?PHP if($trainer[$i]['member_id']==$member['member_keeper']) {
+        echo 'selected';
+    }
 
-  <form role="form" method="post" action="index.php?content=accout&action=edit" enctype="multipart/form-data">
-    <div class="row">
-        <div class="col-lg-9">
-            <div class="row">
-                <div class="col-lg-3">
-                    <div class="form-group">
-                        <label>username <font color="#F00"><b>*</b></font></label>
-                        <input id="member_username" name="member_username" class="form-control"
-                            value="<?php echo $member['member_username']?>" autocomplete="off">
-                    </div>
-                </div>
-                <div class="col-lg-3">
-                    <div class="form-group">
-                        <label>password <font color="#F00"><b>*</b></font></label>
-                        <input id="member_password" name="member_password" class="form-control"
-                            value="<?php echo $member['member_password']?>" autocomplete="off">
-                    </div>
-                </div>
-                <div class="col-lg-3">
-                    <div class="form-group">
-                        <label>ประเภท <font color="#F00"><b>*</b></font></label>
-                        <input id="member_typemember" name="member_typemember" class="form-control"
-                            value="<?php echo $member['member_typemember']?>" autocomplete="off">
-                    </div>
-                </div>
-                <div class="col-lg-3">
-                    <div class="form-group">
-                        <label>เพศ <font color="#F00"><b>*</b></font></label>
-                        <input id="member_sex" name="member_sex" class="form-control"
-                            value="<?php echo $member['member_sex']?>" autocomplete="off">
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-4">
-                    <div class="form-group">
-                        <label>ชื่อ <font color="#F00"><b>*</b></font></label>
-                        <input id="member_firstname" name="member_firstname" class="form-control"
-                            value="<?php echo $member['member_firstname']?>" autocomplete="off">
-                    </div>
-                </div>
-                <div class="col-lg-4">
-                    <div class="form-group">
-                        <label>นามสกุล <font color="#F00"><b>*</b></font></label>
-                        <input id="member_lastname" name="member_lastname" class="form-control"
-                            value="<?php echo $member['member_lastname']?>" autocomplete="off">
-                    </div>
-                </div>
-                <div class="col-lg-4">
-                    <div class="form-group">
-                        <label>เบอร์โทรศัพท์ </label>
-                        <input id="member_tel" name="member_tel" class="form-control"
-                            value="<?php echo $member['member_tel']?>" autocomplete="off">
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="form-group">
-                        <label> วันเกิด </label>
-                        <input id="member_birthday" name="member_birthday" class="form-control"
-                            value="<?php echo $member['member_birthday']?>" autocomplete="off">
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-6">
-                    <div class="form-group">
-                        <label> วันที่เริ่ม </label>
-                        <input id="member_start" name="member_start" class="form-control"
-                            value="<?php echo $member['member_start']?>" autocomplete="off">
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="form-group">
-                        <label> วันที่หมดอายุ </label>
-                        <input id="member_expiry" name="member_expiry" class="form-control"
-                            value="<?php echo $member['member_expiry']?>" autocomplete="off">
-                    </div>
-                </div>
-            </div>
-        </div>
+    ?>><?PHP echo $trainer[$i]['member_firstname'] .' '. $trainer[$i]['member_lastname'];
+    ?></option><?PHP
+}
 
-    </div>
-
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="form-group">
-                <label>ที่อยู่ <font color="#F00"><b>*</b></font> </label>
-                <textarea type="text" id="member_address" name="member_address"
-                    class="form-control"><?php echo $member['member_address']?></textarea>
-                <p class="help-block">Example : 271/55.</p>
-            </div>
-        </div>
-    </div>
-    <!-- /.row (nested) -->
-
-    <div class="row">
-        <div class="col-lg-3">
-            <div class="form-group">
-                <?PHP  $trainer =  $member_model->getUserByTrainer();
-    //   print_r ($trainer);  
-      ?>
-
-                <select class="form-control  custom-select" id="member_keeper" name="member_keeper">
-                    <?PHP 
-                    for ($i=0; $i < count($trainer); $i++) { ?>
-                    <option value="<?PHP echo $trainer[$i]['member_id'];?>" <?PHP if($trainer[$i]['member_id'] == $member['member_keeper']){echo 'selected';}?>> 
-                    <?PHP echo $trainer[$i]['member_firstname']
-                        .' '.  $trainer[$i]['member_lastname'];?>
-                    </option>
-                    <?PHP }?>
-                </select>
-
-
-
-
-            </div>
-        </div>
-
-    </div>
-
-    <div align="right">
-        <input type="hidden" id="member_id" name="member_id" value="<?php echo $member['member_id'] ?>" />
-        <input type="hidden" id="member_status" name="member_status" value="<?php echo $member['member_status'] ?>" />
-        <button type="button" class="btn btn-default" onclick="window.location='?content=accout';">ย้อนกลับ</button>
-        <button type="reset" class="btn btn-primary">ล้างข้อมูล</button>
-        <button name="submit" type="submit" class="btn btn-success">บันทึกข้อมูล</button>
-    </div>
-</form>
-  </div>
-</div>
+?></select></div></div></div><div align="right"><input type="hidden"id="member_id"name="member_id"value="<?php echo $member['member_id'] ?>"/><input type="hidden"id="member_status"name="member_status"value="<?php echo $member['member_status'] ?>"/><button type="button"class="btn btn-default"onclick="window.location='?content=accout';">ย้อนกลับ</button><button type="reset"class="btn btn-primary">ล้างข้อมูล</button><button name="submit"type="submit"class="btn btn-success">บันทึกข้อมูล</button></div></form></div></div>
