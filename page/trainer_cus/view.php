f44c9dedf338fe89f9e7c6b23a02ca8a8c7e9442
Warayut Taekrathok <warayut2555@gmail.com>
2019-03-20 00:23:43 +0700

    Update view.php

--- page/trainer_cus/view.php
+++ page/trainer_cus/view.php
@@ -1,30 +1,13 @@
-<h1></h1>
-<div class="card">
-  <div class="card-header">
-  
-  <h1>
-  ข้อมูลลูกค้า
-  </h1>
-  </div>
-<table class="table table-striped">
-  <thead>
-    <tr>
-      <th>#</th>
-      <th>ชื่อ - นามสกุล</th>  
-      <th>เบอร์โทรศัพท์</th>
-      <th>ประเภทผุ้ใช้งาน</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php 
-    for($i=0; $i < count($member); $i++){
-    ?>
-    <tr>
-      <td><?php echo $i+1; ?></td>
-      <td class="text-left"><?php echo $member[$i]['member_firstname']; ?> <?php echo $member[$i]['member_lastname']; ?> </td>
-      <td><?php echo $member[$i]['member_tel']; ?></td>
-      <td class="center"><?php echo $member[$i]['member_typemember']; ?></td>
-    </tr>
-    <?php } ?>
-  </tbody>
-</table>  
+<h1></h1><div class="card"><div class="card-header"><h1>ข้อมูลลูกค้า </h1></div><table class="table table-striped"><thead><tr><th>#</th><th>ชื่อ - นามสกุล</th><th>เบอร์โทรศัพท์</th><th>ประเภทผุ้ใช้งาน</th></tr></thead><tbody><?php for($i=0;
+$i < count($member);
+
+$i++) {
+  ?><tr><td><?php echo $i+1;
+  ?></td><td class="text-left"><?php echo $member[$i]['member_firstname'];
+  ?><?php echo $member[$i]['member_lastname'];
+  ?></td><td><?php echo $member[$i]['member_tel'];
+  ?></td><td class="center"><?php echo $member[$i]['member_typemember'];
+  ?></td></tr><?php
+}
+
+?></tbody></table>
